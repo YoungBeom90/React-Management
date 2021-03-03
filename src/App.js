378a677d409a26cc921513d37bbfeb64e8c10503
ybkim <ybkim@mind-one.co.kr>
2021-03-03 13:10:20 +0900

    Create Project App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers= [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '김영범',
+  'birthday' : '900216',
+  'gender' : '남자',
+  'job' : '직장인'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '홍길동',
+    'birthday' : '920222',
+    'gender' : '남자',
+    'job' : '예술인'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '김철수',
+    'birthday' : '930222',
+    'gender' : '남자',
+    'job' : '대학생'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            customers.map(c => {
+              return(
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              )
+            })
+          }
+        </TableBody>
+      </Table>
+    );
+  }
 }
 
 export default App;
